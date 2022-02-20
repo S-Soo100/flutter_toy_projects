68202a86326315a96615f683dc6b2751b5201244
Arthur <bss.rol20@gmail.com>
2022-02-21 01:12:03 +0900

    commit me

--- lib/screen/shopping_cart_provider_home.dart
+++ lib/screen/shopping_cart_provider_home.dart
@@ -8,7 +8,8 @@ class ShoppingCartProviderHome extends StatefulWidget {
   ShoppingCartProviderHome({Key? key}) : super(key: key);
 
   @override
-  State<ShoppingCartProviderHome> createState() => _ShoppingCartProviderHomeState();
+  State<ShoppingCartProviderHome> createState() =>
+      _ShoppingCartProviderHomeState();
 }
 
 class _ShoppingCartProviderHomeState extends State<ShoppingCartProviderHome> {
@@ -23,49 +24,60 @@ class _ShoppingCartProviderHomeState extends State<ShoppingCartProviderHome> {
       appBar: AppBar(
         title: const Text('쇼핑카트 프로바이더 활용 예시'),
         actions: [
-
-          IconButton(onPressed: () {
-            Navigator.pushNamed(context, 'home');
-          }, icon: const Icon(Icons.home))
+          IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, 'home');
+              },
+              icon: const Icon(Icons.home))
         ],
       ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(8),
-        itemCount: context.watch<ShoppingCart>().count+1 ,
-        itemBuilder: (BuildContext context, int index) {
-          if (index == 0)
-            return HeaderTile(title: '총 가격 : ${(context.watch<ShoppingCart>().count)*1000} 원', subtitle: '총 ${context.read<ShoppingCart>().count} 개');
-          return ShoppingListTile(
-            title: '${context.watch<ShoppingCart>().products.elementAt(index-1)}',
-            subtitle: '가격 : 1,000원', item: context.watch<ShoppingCart>().products.elementAt(index-1),
-          );
-        },
+      body: Scrollbar(
+        child: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: context.watch<ShoppingCart>().count + 1,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return HeaderTile(
+                  title:
+                      '총 가격 : ${(context.watch<ShoppingCart>().count) * 1000} 원',
+                  subtitle: '총 ${context.read<ShoppingCart>().count} 개');
+            }
+            return ShoppingListTile(
+              title:
+                  context.watch<ShoppingCart>().products.elementAt(index - 1),
+              subtitle: '가격 : 1,000원',
+              item: context.watch<ShoppingCart>().products.elementAt(index - 1),
+            );
+          },
+        ),
       ),
-      floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              heroTag: null,
-              onPressed: () => context.read<ShoppingCart>().addItem('coffee'),
-              child: const Text('커피'),),
-            const SizedBox(width: 10),
-            FloatingActionButton(
-              heroTag: null,
-              onPressed: () => context.read<ShoppingCart>().addItem('avocado'),
-              child: const Text('아보\n카도'),),
-            const SizedBox(width: 10),
-            FloatingActionButton(
-              heroTag: null,
-              onPressed: () => context.read<ShoppingCart>().addItem('milk'),
-              child: const Text('우유'),),
-            const SizedBox(width: 10),
-            FloatingActionButton(
-              backgroundColor:  Colors.red,
-              heroTag: null,
-              onPressed: () => context.read<ShoppingCart>().purchase(),
-              child: const Text('구매'),)
-          ]
-      ),
+      floatingActionButton:
+          Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+        FloatingActionButton(
+          heroTag: null,
+          onPressed: () => context.read<ShoppingCart>().addItem('coffee'),
+          child: const Text('커피'),
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          heroTag: null,
+          onPressed: () => context.read<ShoppingCart>().addItem('avocado'),
+          child: const Text('아보\n카도'),
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          heroTag: null,
+          onPressed: () => context.read<ShoppingCart>().addItem('milk'),
+          child: const Text('우유'),
+        ),
+        const SizedBox(width: 10),
+        FloatingActionButton(
+          backgroundColor: Colors.red,
+          heroTag: null,
+          onPressed: () => context.read<ShoppingCart>().purchase(),
+          child: const Text('구매'),
+        )
+      ]),
     );
   }
 }
