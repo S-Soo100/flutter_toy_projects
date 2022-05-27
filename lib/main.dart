343df693a7e940db8445d1402b6ce1e64a86c739
백승수 <baek@baegseungsus-Mac-mini.local>
2022-05-27 10:54:58 +0900

    ios_googleSignin

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:my_practice_app/firebase_options.dart';
 import 'package:my_practice_app/provider/shopping_cart_provider.dart';
 import 'package:my_practice_app/screen/firebase/email_login_page.dart';
 import 'package:my_practice_app/screen/firebase/google_login_page.dart';
@@ -21,7 +22,9 @@ import 'dart:ffi';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MultiProvider(providers: [
       ChangeNotifierProvider(create: (_) => Counter()),
